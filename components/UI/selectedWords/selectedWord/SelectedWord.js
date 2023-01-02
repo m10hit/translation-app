8e37bd505da2c7663f8d68c36aa044a8be167d94
Mohit Chauhan <54078588+m10hit@users.noreply.github.com>
2023-01-02 13:55:56 +0530

    Update SelectedWord

--- components/UI/selectedWords/selectedWord/SelectedWord.js
+++ components/UI/selectedWords/selectedWord/SelectedWord.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SelectedWord = ({ word }) => {
   return (
-    <div className="border-gray-250 border-b mb-2">
+    <div className="border-gray-250 mb-2">
       <span className="px-4 py-2 m-2 mr-1 rounded-full border-2 border-gray-250 font-semibold text-sm flex align-center w-max cursor-pointer transition duration-300 ease">
         {word}
       </span>
