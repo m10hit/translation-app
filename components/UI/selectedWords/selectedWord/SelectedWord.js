75a9b2f5bdd4cd4299837a41477d0010c02973d5
Mohit Chauhan <54078588+m10hit@users.noreply.github.com>
2023-01-02 14:01:02 +0530

    Include horizontal line

--- components/UI/selectedWords/selectedWord/SelectedWord.js
+++ components/UI/selectedWords/selectedWord/SelectedWord.js
@@ -6,6 +6,7 @@ const SelectedWord = ({ word }) => {
       <span className="px-4 py-2 m-2 mr-1 rounded-full border-2 border-gray-250 font-semibold text-sm flex align-center w-max cursor-pointer transition duration-300 ease">
         {word}
       </span>
+      <hr />
     </div>
   );
 };
